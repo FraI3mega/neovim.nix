5bac7906758698e21eda9d204e8af4c172a3cc80
FraI3mega <projeapp2@gmail.com>
2024-10-27 01:25:00 +0200

    tmp name change

--- flake.nix
+++ flake.nix
@@ -342,7 +342,7 @@
     packageDefinitions = {
       # These are the names of your packages
       # you can include as many as you wish.
-      nvim = {pkgs, ...}: {
+      nvim-new = {pkgs, ...}: {
         # they contain a settings set defined above
         # see :help nixCats.flake.outputs.settings
         settings = {
@@ -374,7 +374,7 @@
     };
     # In this section, the main thing you will need to do is change the default package name
     # to the name of the packageDefinitions entry you wish to use as the default.
-    defaultPackageName = "nvim";
+    defaultPackageName = "nvim-new";
   in
     # see :help nixCats.flake.outputs.exports
     forEachSystem (
