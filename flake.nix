a32403e6738846cf698c22c912c47984313bc208
FraI3mega <projeapp2@gmail.com>
2024-11-07 21:07:10 +0100

    rename binary

--- flake.nix
+++ flake.nix
@@ -369,7 +369,7 @@
     packageDefinitions = {
       # These are the names of your packages
       # you can include as many as you wish.
-      nvim-new = {pkgs, ...}: {
+      nvim = {pkgs, ...}: {
         # they contain a settings set defined above
         # see :help nixCats.flake.outputs.settings
         settings = {
@@ -401,7 +401,7 @@
     };
     # In this section, the main thing you will need to do is change the default package name
     # to the name of the packageDefinitions entry you wish to use as the default.
-    defaultPackageName = "nvim-new";
+    defaultPackageName = "nvim";
   in
     # see :help nixCats.flake.outputs.exports
     forEachSystem (
