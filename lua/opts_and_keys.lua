ca90bb8b9b7e5b71e229eaac97f294eaa77cb393
FraI3mega <projeapp2@gmail.com>
2024-11-08 19:10:47 +0100

    fix autoshift

--- lua/opts_and_keys.lua
+++ lua/opts_and_keys.lua
@@ -50,7 +50,7 @@ vim.o.smartindent = true
 vim.o.autoindent = true
 vim.o.tabstop = 4
 -- vim.o.softtabstop = 4
--- vim.o.shiftwidth = 4
+vim.o.shiftwidth = 4
 -- stops line wrapping from being confusing
 vim.o.breakindent = true
 
