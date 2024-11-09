d198ad5b21246fdefa294b61b4b2d87198fe2158
FraI3mega <projeapp2@gmail.com>
2024-11-10 00:37:14 +0100

    fix blink.cmp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,5 +1,5 @@
----packadd + after/plugin
----@type fun(names: string[]|string)
+---@module 'blink.cmp'
+---@type blink.cmp.Config
 
 -- NOTE: packadd doesnt load after directories.
 -- hence, the above function that you can get from luaUtils that exists to make that easy.
@@ -10,7 +10,18 @@ require("blink-cmp").setup({
   trigger = { signature_help = { enabled = true } },
   windows = {
     documentation = { auto_show = true },
-    autocomplete = { draw = "reversed" },
+    autocomplete = { draw = { columns = { { "label", "label_description", gap = 1 }, { "kind_icon" } } } },
   },
   fuzzy = { prebuilt_binaries = { download = false } },
+  sources = {
+    -- add lazydev to your completion providers
+    completion = {
+      enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
+    },
+    providers = {
+      -- dont show LuaLS require statements when lazydev has items
+      lsp = { fallback_for = { "lazydev" } },
+      lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+    },
+  },
 })
