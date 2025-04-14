ae19b7ddf04cbea6c6742ea971910afc82885f3f
FraI3mega <projeapp2@gmail.com>
2025-04-14 18:48:15 +0200

    upd

--- flake.nix
+++ flake.nix
@@ -357,9 +357,6 @@
       # in your lua config via
       # vim.g.python3_host_prog
       # or run from nvim terminal via :!<packagename>-python3
-      extraPython3Packages = {
-        test = _: [];
-      };
       # populates $LUA_PATH and $LUA_CPATH
       extraLuaPackages = {
         general = [(lp: with lp; [magick])];
